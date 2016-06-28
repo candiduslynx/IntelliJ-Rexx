9879cc647e164ea2e6acb1c69f14a7c2bba671af
candiduslynx <candiduslynx@gmail.com>
2016-06-28 12:02:58 +0300

    Add icons pointer

---
+++ src/java/com/intellij/rexx/Icons.java
@@ -0,0 +1,9 @@
+package java.com.intellij.rexx;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class Icons {
+	public static final Icon REXX_ICON = IconLoader.getIcon("/icons/com/intellij/rexx/Rexx.png");
+}
