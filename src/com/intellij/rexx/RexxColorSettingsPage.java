17aeb6b95354d9ca1a2780e4e55d4c9db595c7e6
candiduslynx <candiduslynx@gmail.com>
2016-07-02 00:33:41 +0300

    Update color settings page

--- src/com/intellij/rexx/RexxColorSettingsPage.java
+++ src/com/intellij/rexx/RexxColorSettingsPage.java
@@ -54,26 +54,27 @@ public class RexxColorSettingsPage implements ColorSettingsPage {
 	public String getDemoText() {
 		return
 			"/* REXX */\n" +
-			"/* some sort of comment */\n" +
-			"/* %include somemod */\n" +
-			"\n" +
-			"mainpgm:\n" +
-			"    parse upper arg input, setting ;\n" +
-			"    if length(input) > 12 then\n" +
-			"        say \"too long: <\" || input || '> is longer that 12 characters!'\n" +
-			"    else do\n" +
-			"        parse var input i1 i2 i3\n" +
-			"        i1 = strip(i1)\n" +
-			"        i2 = strip(i2)\n" +
-			"        i3 = strip(i3)\n" +
-			"        if i3 = '' then\n" +
-			"            i3 = .SIGL\n" +
-			"        while (i1 > i2) do\n" +
-			"            i1 = i1 + length(i3)\n" +
-			"            say i1\n" +
-			"        end\n" +
-			"    end\n" +
-			"return 0";
+				"/* some sort of comment */\n" +
+				"/* %include somemod */\n" +
+				"\n" +
+				"mainpgm:\n" +
+				"    parse upper arg input, setting ;\n" +
+				"    if length(input) > 12 then\n" +
+				"        say \"too long: <\" || input || '> is longer than 12 characters!'\n" +
+				"    else do\n" +
+				"        parse var input i1 i2 i3\n" +
+				"        i1 = strip(i1)\n" +
+				"        i2 = strip(i2)\n" +
+				"        i3 = strip(i3)\n" +
+				"        if i3 = '' then\n" +
+				"            i3 = .SIGL\n" +
+				"        while (i1 > i2) do\n" +
+				"            i1 = i1 + length(i3) + , /*<--continuation*/\n" +
+				"                    15\n" +
+				"            say i1\n" +
+				"        end\n" +
+				"    end\n" +
+				"return 0";
 	}
 
 	@NotNull
--- test/01.rexx
+++ test/01.rexx
@@ -5,7 +5,7 @@
 mainpgm:
     parse upper arg input, setting ;
     if length(input) > 12 then
-        say "too long: <" || input || '> is longer that 12 characters!'
+        say "too long: <" || input || '> is longer than 12 characters!'
     else do
         parse var input i1 i2 i3
         i1 = strip(i1)
@@ -14,7 +14,8 @@ mainpgm:
         if i3 = '' then
             i3 = .SIGL
         while (i1 > i2) do
-            i1 = i1 + length(i3)
+            i1 = i1 + length(i3) + , /*<--continuation*/
+                    15
             say i1
         end
     end
